397fc7eea8962d02ed6fbb080c275f7e675bd62b
huangxiangyun <ashleigh8327@163.com>
2019-09-19 17:33:06 +0800

    初始化flutter项目

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() =>runApp(MyApp());
+
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+        title: '百姓生活+',
+        debugShowCheckedModeBanner: false, //右上角debug字样去掉
+        theme: ThemeData(
+        ),
+      ),
+    );
+  }
+}
