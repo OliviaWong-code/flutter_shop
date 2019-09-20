5fc3a86804c4d1b41fa16320ac9ee35437d69dcc
huangxiangyun <ashleigh8327@163.com>
2019-09-20 17:25:46 +0800

    引入 get post请求

---
+++ lib/config/httpHeaders.dart
@@ -0,0 +1,17 @@
+const httpHeaders={
+'Accept': 'application/json, text/plain, */*',
+'Accept-Encoding': 'gzip, deflate, br',
+'Accept-Language': 'zh-CN,zh;q=0.9',
+'Cache-Control': 'no-cache',
+'Connection': 'keep-alive',
+'Content-Length': '2',
+'Content-Type': 'application/json',
+'Cookie': '_ga=GA1.2.1205252704.1568963061; _gid=GA1.2.841507671.1568963061; _gat=1; Hm_lvt_022f847c4e3acd44d4a2481d9187f1e6=1568963061; MEIQIA_TRACK_ID=1R5hraJfrvjmV3xYKkOo6OMKXn6; MEIQIA_VISIT_ID=1R5hrYNpezqICxO0aEfmJIA77u9; SERVERID=1fa1f330efedec1559b3abbcb6e30f50|1568963075|1568963061; Hm_lpvt_022f847c4e3acd44d4a2481d9187f1e6=1568963076',
+'Host': 'time.geekbang.org',
+'Origin': 'https://time.geekbang.org',
+'Pragma': 'no-cache',
+'Referer': 'https://time.geekbang.org/',
+'Sec-Fetch-Mode': 'cors',
+'Sec-Fetch-Site': 'same-origin',
+'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+};
